aec10cfc04a87664c6e194ad20c565edabc2d745
tutam <damtutam@gmail.com>
2024-12-16 17:13:22 +0700

    feat: api for getAll, update accounts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,11 +6,12 @@ import notFoundMiddleware from "../lib/middlewares/not-found.middleware";
 import filterExceptionMiddleware from "../lib/middlewares/filter-exception.middleware";
 
 import users from './users.r';
+import { API_VERSION } from "../shared/constants/api-version.constant";
 
 const initRoutes = async (app: Express) => {
     app.use(loggerMiddleware);
     app.use(authenticationMiddleware);
-    app.use('/users', users);
+    app.use(`/api/${API_VERSION.V1}/users`, users);
     app.use('*', notFoundMiddleware);
     app.use(filterExceptionMiddleware);
 }

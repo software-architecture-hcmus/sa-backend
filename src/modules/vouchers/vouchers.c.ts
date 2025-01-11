86f9db71b1160430113136f5872708b4f5a7f36a
21120538 <luuvinhquangqg@gmail.com>
2025-01-12 00:36:31 +0700

    feat: ga voucher

--- src/modules/vouchers/vouchers.c.ts
+++ src/modules/vouchers/vouchers.c.ts
@@ -6,6 +6,7 @@ import AppDataSource from "../../database/data-source";
 import { TripleStatus } from "../../database/enums/triple-status.enum";
 import { Transaction } from "../../database/entities/transactions.entity";
 import { CustomerVoucher } from "../../database/entities/customer_vouchers.entity";
+import { Voucher } from "../../database/entities/voucher.entity";
 
 class VoucherController {
     async test(req: Request, res: Response, next: NextFunction) {
@@ -21,7 +22,8 @@ class VoucherController {
     async giveAway(req: Request, res: Response, next: NextFunction) {
         const _req = req as CustomUserRequest;
         const body = _req.body;
-        const {customer_voucher_id, sender_id, receiver_email} = body;
+        const sender_id = _req.user.uid;
+        const {customer_voucher_id, receiver_email} = body;
         if (!customer_voucher_id || !sender_id || !receiver_email) {
             return res.status(400).json({
                 ok: false,
@@ -45,11 +47,13 @@ class VoucherController {
 
     async acceptVoucher(req: Request, res:Response, next: NextFunction) {
         const _req = req as CustomUserRequest;
-        // const userID = "5WNJfnPfQcPZYMliKu51APahj9o2";
         const userID = _req.user?.uid;
         const {ga_token} = _req.query as {ga_token: string};
         if (!userID || !ga_token) {
-            return next(new Error("Invalid data"));
+            return res.status(400).json({
+                ok: false,
+                message: "Invalid data",
+            });
         }
 
         await AppDataSource.transaction(async (entityManager) => {
@@ -57,27 +61,42 @@ class VoucherController {
             //Get the transaction by token
             const transactionEntity = await entityManager.findOne(Transaction, { where: { token: ga_token }, relations: ['customer_voucher'] });
             if (!transactionEntity) {
-                return next(new Error("Transaction not found"));
+                return res.status(404).json({
+                    ok: false,
+                    message: "Transaction not found",
+                });
             }
 
             //check receiver id & status
             if (transactionEntity.receiver_id !== userID) {
-                return next(new Error("Receiver ID does not match"));
+                return res.status(400).json({
+                    ok: false,
+                    message: "Receiver ID does not match",
+                });
             }
             if (transactionEntity.status !== TripleStatus.PENDING) {
-                return next(new Error("Transaction status is not pending"));
+                return res.status(400).json({
+                    ok: false,
+                    message: "Transaction status is not pending",
+                });
             }
 
             //The GA email will expire after 3 days, so check
             const createdAt = new Date(transactionEntity.createdAt);
             const currentDate = new Date();
             if ((currentDate.getTime() - createdAt.getTime()) / (1000 * 3600 * 24) > 3) {
-                return next(new Error("Transaction has expired"));
+                return res.status(400).json({
+                    ok: false,
+                    message: "Transaction has expired",
+                });
             }
 
             const customerVoucher = transactionEntity.customer_voucher;
             if (!customerVoucher || customerVoucher.code) {
-                return next(new Error("Customer voucher not found or voucher was claimed"));
+                return res.status(400).json({
+                    ok: false,
+                    message: "Customer voucher not found or voucher was claimed",
+                });
             }
 
             //If everything ok, begin transaction
@@ -91,10 +110,40 @@ class VoucherController {
             
             return res.status(200).json({
                 ok: true,
-                message: "SUCCESS",
+                message: "Receive give away voucher success",
             });
         });
+    }
 
+    
+    async getCustomerVoucher(req: Request, res: Response, next: NextFunction) {
+        const _req = req as CustomUserRequest;
+        try {
+            const customerVouchers = await AppDataSource.getRepository(CustomerVoucher).find({
+                where: { customer_id: _req.user?.uid },
+                relations: ['voucher'],
+                select: {
+                    id: true,
+                    customer_id: true,
+                    code: true,
+                    voucher: {
+                        id: true,
+                        image: true,
+                        value: true,
+                        description: true,
+                        expiry_date: true,
+                        status: true,
+                    }
+                }
+            });
+
+            return res.status(200).json({
+                ok: true,
+                data: customerVouchers,
+            });
+        } catch (error) {
+            next(error);
+        }
     }
 }
 

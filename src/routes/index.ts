c05a895d45b896c5843b90df1ee58a8ca28d83f6
Le Sang <73180106+Flife11@users.noreply.github.com>
2025-01-13 19:38:14 +0700

    feat: update game (#23)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -13,10 +13,13 @@ import voucher from './voucher.r'
 import favourites from './favourite.r';
 import notifications from './notifications.r';
 import { API_VERSION } from "../shared/constants/api-version.constant";
+import { uploadMultiple } from "../lib/middlewares/upload.middleware";
+import uploadController from '../shared/upload/upload';
 
 const initRoutes = async (app: Express) => {
     app.use(loggerMiddleware);
     app.use(authenticationMiddleware);
+    app.use(`/api/${API_VERSION.V1}/upload`, uploadMultiple('files'), uploadController.upload);
     app.use(`/api/${API_VERSION.V1}/users`, users);
     app.use(`/api/${API_VERSION.V1}/events`, events);
     app.use(`/api/${API_VERSION.V1}/games`, game);
